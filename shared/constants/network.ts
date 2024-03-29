a91d7ae6369f612b5b9bb9dad424536b7a107336
Matteo Scurati <matteo.scurati@gmail.com>
2024-03-29 16:54:12 +0100

    feat: :construction: code improvements after a PR review

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -299,7 +299,7 @@ export const CURRENCY_SYMBOLS = {
   ONE: 'ONE',
 } as const;
 
-const CHAINLIST_CURRENCY_SYMBOLS_MAP = {
+export const CHAINLIST_CURRENCY_SYMBOLS_MAP = {
   ...CURRENCY_SYMBOLS,
   BASE: 'ETH',
   LINEA_MAINNET: 'ETH',
